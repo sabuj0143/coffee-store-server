e62c379a120abc0ceb89079e383efffb1ce29650
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-09 08:53:06 +0600

    Monogdb added

--- index.js
+++ index.js
@@ -11,19 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMester
-// gdOQ72XOJHWVV87u
-
-
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
 
 
 // --------------------------------------------------------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1vjhzt.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
-// const uri = "mongodb+srv://coffeeMester:gdOQ72XOJHWVV87u@cluster0.s1vjhzt.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,12 +29,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
